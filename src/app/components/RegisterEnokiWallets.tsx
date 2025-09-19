b6936b1ab9cac52c8a40cfebfbbabc1e17f9da3b
후니 <hipark@huniui-MacBookPro.local>
2025-09-20 05:59:37 +0900

    Refactor: env vals

--- src/app/components/RegisterEnokiWallets.tsx
+++ src/app/components/RegisterEnokiWallets.tsx
@@ -10,10 +10,10 @@ export default function RegisterEnokiWallets() {
 		if (!isEnokiNetwork(network)) return;
 
 		const { unregister } = registerEnokiWallets({
-			apiKey: process.env.NEXT_PUBLIC_ENOKI_API_KEY || 'enoki_public_eadb825c5c90c588b048d22404a5a4b2',
+			apiKey: 'enoki_public_eadb825c5c90c588b048d22404a5a4b2',
 			providers: {
 				google: {
-					clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || '347337325039-9ics8t8klbs3ukov4o6u6ps5fre3t7t3.apps.googleusercontent.com',
+					clientId: '347337325039-9ics8t8klbs3ukov4o6u6ps5fre3t7t3.apps.googleusercontent.com',
 				},
 				// facebook: {
 				// 	clientId: 'YOUR_FACEBOOK_CLIENT_ID',
