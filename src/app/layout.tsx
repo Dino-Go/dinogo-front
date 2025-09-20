d71a0ed322c3693dd7c61c8b7a1c55ad3f9b5d70
후니 <hipark@huniui-MacBookPro.local>
2025-09-20 15:52:09 +0900

    Chkpt

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 import '@mysten/dapp-kit/dist/index.css';
 
 // Providers
-import { Providers } from "@/app/components/Providers";
+import { Providers } from "@/app/components/providers";
 
 const geistSans = Geist({
 	variable: "--font-geist-sans",
